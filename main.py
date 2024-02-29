6866af09eb30513ed78ad4495102de943b58099f
Kirill <okaydiscord@mail.ru>
2024-02-29 21:43:50 +0300

    доработана форма авторизации

--- main.py
+++ main.py
@@ -1,117 +1,20 @@
-from datetime import datetime
-
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
+from flask_login import login_user, LoginManager
 
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
-from forms.users import RegisterForm
+from forms.users import RegisterForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager(app)
 
 
-def get_users_data():
-    users_data = [
-        {
-            "surname": "Scott",
-            "name": "Ridley",
-            "age": 21,
-            "position": "captain",
-            "speciality": "research engineer",
-            "address": "module_1",
-            "email": "scott_chief@mars.org",
-        },
-        {
-            "surname": "Weer",
-            "name": "Andy",
-            "age": 23,
-            "position": "science pilot",
-            "speciality": "drone pilot",
-            "address": "module_2",
-            "email": "andy_story@mars.org",
-        },
-        {
-            "surname": "Watney",
-            "name": "Mark",
-            "age": 22,
-            "position": "mission specialist",
-            "speciality": "astrogeologist",
-            "address": "module_3",
-            "email": "mark3@mars.org",
-        },
-        {
-            "surname": "Kapoor",
-            "name": "Venkata",
-            "age": 25,
-            "position": "flight engineer",
-            "speciality": "cyberengineer",
-            "address": "module_4",
-            "email": "kapoor_astro@mars.org",
-        },
-    ]
-    return users_data
-
-
-def create_users():
+@login_manager.user_loader
+def load_user(user_id):
     db_sess = db_session.create_session()
-    users = get_users_data()
-    for user_data in users:
-        user = User(**user_data)
-        db_sess.add(user)
-    db_sess.commit()
-
-
-def get_jobs_data():
-    jobs_data = [
-        {
-            "team_leader_id": 1,
-            "job": "Deployment of residential modules 1 and 2",
-            "work_size": 15,
-            "collaborators": "2, 3",
-            "start_date": datetime.now(),
-            "is_finished": False,
-        },
-        {
-            "team_leader_id": 2,
-            "job": "Exploration of mineral sources",
-            "work_size": 15,
-            "collaborators": "4, 3",
-            "start_date": datetime.now(),
-            "is_finished": False,
-        },
-        {
-            "team_leader_id": 3,
-            "job": "Development of management system",
-            "work_size": 25,
-            "collaborators": "5",
-            "start_date": datetime.now(),
-            "is_finished": False,
-        },
-        {
-            "team_leader_id": 4,
-            "job": "Fix ventilation system",
-            "work_size": 20,
-            "collaborators": "2, 5",
-            "start_date": datetime.now(),
-            "is_finished": True,
-        },
-    ]
-    return jobs_data
-
-
-def create_jobs():
-    db_sess = db_session.create_session()
-    jobs = get_jobs_data()
-    for job_data in jobs:
-        job = Jobs(**job_data)
-        db_sess.add(job)
-    db_sess.commit()
-
-
-def add_data_to_db():
-    create_users()
-    create_jobs()
+    return db_sess.get(User, user_id)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -135,9 +38,18 @@ def register():
     return render_template("register.html", title="Регистрация", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", title="Авторизация")
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        flash("Неправильный логин или пароль", "danger")
+        return render_template("login.html", form=form)
+    return render_template("login.html", title="Авторизация", form=form)
 
 
 @app.route("/")
