9a2b5c671e80fa6a308a7356ac911259c0c7179e
Kirill <okaydiscord@mail.ru>
2024-04-23 22:32:10 +0300

    добавлен файл фабрики для работы с БД

--- data/db_session.py
+++ data/db_session.py
@@ -1,3 +1,5 @@
+import os
+
 import sqlalchemy as sa
 import sqlalchemy.ext.declarative as dec
 import sqlalchemy.orm as orm
@@ -7,6 +9,9 @@ SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
+DB_POOL_SIZE = int(os.getenv("DB_POOL_SIZE", "100"))
+WEB_CONCURRENCY = int(os.getenv("WEB_CONCURRENCY", "2"))
+POOL_SIZE = max(DB_POOL_SIZE // WEB_CONCURRENCY, 5)
 
 def global_init(db_file):
     global __factory
@@ -15,7 +20,7 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise ValueError("Необходимо указать файл БД")
     conn_str = f"sqlite:///{db_file.strip()}?check_same_tread=False"
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False, pool_size=POOL_SIZE, max_overflow=0)
     __factory = orm.sessionmaker(bind=engine)
     print(f"Подключение к БД: {conn_str} ...")
     SqlAlchemyBase.metadata.create_all(engine)
