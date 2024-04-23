d4d9706630149ca0a95290ac9225b3679fec055c
yanashum <yanashum@mail.ru>
2024-04-23 22:02:22 +0300

    доработан конфиг приложения

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'data.db')}"
 SECRET_KEY = "yandexlyceum_secret_key"
 GEO_API_KEY = "40d1649f-0493-4b70-98ba-98533de7710b"
 GEO_API_URL = "http://geocode-maps.yandex.ru/1.x/"
+STATIC_MAPS_URL = 'https://static-maps.yandex.ru/1.x/'
